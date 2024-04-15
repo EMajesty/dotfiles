60ed6b79ca22c49f9ad128ca1a1f6764b5522ecd
EMajesty <lassi.laitinen@protonmail.com>
2024-04-15 21:36:29 +0300

    include home folder

---
+++ .config/nvim/lua/emaj/lazy.lua
@@ -0,0 +1,78 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+--{
+--	'folke/tokyonight.nvim',
+--	lazy = false,
+--	priority = 1000,
+--	opts = {},
+--	config = function()
+--		vim.cmd[[colorscheme tokyonight]]
+--	end,
+--},
+--{
+--    'catppuccin/nvim', 
+--    name = 'catppuccin',
+--    priority = 1000,
+--    config = function()
+--        vim.cmd[[colorscheme catppuccin]]
+--    end,
+--},
+{
+    'sainnhe/gruvbox-material',
+    name = 'gruvbox-material',
+    priority = 1000,
+    config = function()
+        vim.cmd[[colorscheme gruvbox-material]]
+    end,
+},
+{
+	'nvim-telescope/telescope.nvim', tag = '0.1.3',
+	dependencies = { 'nvim-lua/plenary.nvim' }
+},
+{
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+},
+{
+	"tpope/vim-fugitive",
+},
+{
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+},
+{
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl", 
+    opts = {},
+    config = function()
+        require("ibl").setup()
+    end,
+},
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
